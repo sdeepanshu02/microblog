5b2aca1665c15f328d4983435f9f35c668c40f37
sdeepanshu02 <U15CO061@coed.svnit.ac.in>
2016-12-12 14:52:45 +0530

    Add HTML Templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World! I am Deepanshu"
+    user = {'nickname': 'Deepanshu'}
+    posts = [ # fake array of posts 
+              { 'author': {'nickname': 'John'}, 'body': 'Beautiful day in Portland!' }, 
+              { 'author': {'nickname': 'Susan'}, 'body': 'The Avengers movie was so cool!' } ]
+    return render_template('index.html', title = 'HOME', user = user, posts=posts)
