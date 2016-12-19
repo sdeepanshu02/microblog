9b73a3533750f19e391347dc0d580ee391591a6a
sdeepanshu02 <U15CO061@coed.svnit.ac.in>
2016-12-19 15:35:48 +0530

    Create Database using Flask-SQLAlchemy extension

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ OPENID_PROVIDERS = [ {'name': 'Google', 'url': 'https://www.google.com/accounts/
                      {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
                      {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'}, 
                      {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
