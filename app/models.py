ec28a7362049a185afd208aa3775d16c4d4f5f5e
sdeepanshu02 <U15CO061@coed.svnit.ac.in>
2017-01-09 16:19:50 +0530

    Add Search functionality

--- app/models.py
+++ app/models.py
@@ -66,13 +66,25 @@ class User(db.Model):
                 break
             version += 1
         return new_nickname    
-
+    
+    
+import sys
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import flask_whooshalchemy as whooshalchemy
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    __searchable__ = ['body']
+
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
+
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+if enable_search:
+    whooshalchemy.whoosh_index(app, Post)
